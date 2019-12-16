99eaec6befcf1eb5f2cb68c28ebe57c8bc35764f
DmitryMarkov <dmitry.markov@allthings.me>
2019-12-16 17:15:26 +0100

    test: wrote down correct pixel diff

--- test/util/getPixelDiff.js
+++ test/util/getPixelDiff.js
@@ -4,10 +4,12 @@ const PNG = require('pngjs').PNG;
 
 const getPixelDiff = async (img1, img2, viewportName, dir) => {
   const { width, height } = img1;
-  const diff = new PNG({ width, height });
+  const diff = new PNG({ width, height: img2.height });
+
+  const pixelDiff = pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
   fs.writeFileSync(`${dir}/${viewportName}/diff.png`, PNG.sync.write(diff));
 
-  return pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
+  return pixelDiff;
 };
 
 module.exports = getPixelDiff;
