3eed03e671a9982a902a24f1d9f5d75e0d155023
DmitryMarkov <dmitry.markov@allthings.me>
2020-01-15 11:41:15 +0100

    test: updated tests to work form build directory

--- test/util/takeScreenshot.js
+++ test/util/takeScreenshot.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
-const PNG = require('pngjs').PNG;
+const fs = require('fs')
+const PNG = require('pngjs').PNG
 
-const config = require('../config');
-const { env } = config;
+const config = require('../config')
+const { env } = config
 
 const takeScreenshot = async (page, route, viewportName, dir) => {
-  await page.goto(`${env.TEST_URL}:${env.TEST_PORT}/${route}`, { waitUntil: 'networkidle0'});
-  await page.screenshot({ path: `${dir}/${viewportName}/${route}.png`, fullPage: true });
+  await page.goto(`${env.TEST_URL}:${env.TEST_PORT}/${route}`, {
+    waitUntil: 'networkidle0',
+  })
+  await page.screenshot({
+    path: `${dir}/${viewportName}/${route}.png`,
+    fullPage: true,
+  })
 
-  return PNG.sync.read(fs.readFileSync(`${dir}/${viewportName}/${route}.png`));
-};
+  return PNG.sync.read(
+    fs.readFileSync(`${dir}/${viewportName}/${route}.png`)
+  )
+}
 
-module.exports = takeScreenshot;
+module.exports = takeScreenshot
